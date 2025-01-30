e94cb49eeb513c69cd11e341670faf5c26e0bb08
atdotno <93791534+atdotno@users.noreply.github.com>
2025-01-30 20:19:20 +0100

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+
+await channel.QueueDeclareAsync(queue: "helloqueue", durable: false, exclusive: false, autoDelete: false,
+    arguments: null);
+
+
+Console.WriteLine(" Waiting..");
+
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"You just received: '{message}' from the message queue");
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync("helloqueue", autoAck: true, consumer: consumer);
+
+
+Console.ReadLine();
---
+++ Producer/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+
+await channel.QueueDeclareAsync(queue: "helloqueue", durable: false, exclusive: false, autoDelete: false,
+    arguments: null);
+
+
+const string message = "Hello woooorld!";
+var body = Encoding.UTF8.GetBytes(message);
+
+
+await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "helloqueue", body: body);
+Console.WriteLine($" You just sent : '{message}' to the message queue.");
+
+
+
+Console.WriteLine("Press enter to exit");
+Console.ReadLine();
